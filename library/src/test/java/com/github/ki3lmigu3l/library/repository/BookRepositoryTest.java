38c6cfa44cd8038742d2c079a9c33dac2c5f3c15
Ezequiel <ki3lmigu3l@gmail.com>
2024-10-18 21:27:54 -0300

    BookRepositoryTest

--- library/src/test/java/com/github/ki3lmigu3l/library/repository/BookRepositoryTest.java
+++ library/src/test/java/com/github/ki3lmigu3l/library/repository/BookRepositoryTest.java
@@ -2,14 +2,19 @@ package com.github.ki3lmigu3l.library.repository;
 
 import com.github.ki3lmigu3l.library.api.model.Book;
 import com.github.ki3lmigu3l.library.api.repository.BookRepository;
+import com.github.ki3lmigu3l.library.api.service.BookService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +25,8 @@ public class BookRepositoryTest {
 
     @Autowired
     TestEntityManager testEntityManager;
+
+    BookService bookService;
 
     @Autowired
     BookRepository bookRepository;
@@ -43,5 +50,40 @@ public class BookRepositoryTest {
         String isbn = "8576082675";
         boolean existsIsbn = bookRepository.existsByIsbn(isbn);
         assertThat(existsIsbn).isFalse();
+
+    }
+
+    @Test
+    @DisplayName("Deve obter um livro por ID")
+    public void findByIdTest() {
+        Book book = createValidBook();
+        testEntityManager.persist(book);
+
+        Optional<Book> foundBook = bookRepository.findById(book.getId());
+        assertThat(foundBook.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve deletar um livro")
+    public void deleteBookTest () {
+        Book book = Book.builder().id(1l).build();
+        Assertions.assertDoesNotThrow( () -> bookService.delete(book));
+        Mockito.verify(bookRepository, Mockito.times(1)).delete(book);
+    }
+
+    @Test
+    @DisplayName("Deve lancar um erro ao tentar deletar um livro inexistente")
+    public void deleteInvalidBookTest () {
+        Book book = Book.builder().id(1l).build();
+        bookService.delete(book);
+        Mockito.verify(bookRepository, Mockito.times(1)).delete(book);
+    }
+
+    private Book createValidBook () {
+        return Book.builder()
+                .author("Robert C. Martin")
+                .title("Código Limpo")
+                .isbn("8576082675")
+                .build();
     }
 }
