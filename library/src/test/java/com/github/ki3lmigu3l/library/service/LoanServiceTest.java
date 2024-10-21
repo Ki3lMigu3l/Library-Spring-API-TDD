66e39515a20b6bab9cc29cff1d44de30db3e8a94
Ezequiel <ki3lmigu3l@gmail.com>
2024-10-21 20:52:47 -0300

    LoanServiceTest: implementado o teste de buscar por emprestimo/loan by id

--- library/src/test/java/com/github/ki3lmigu3l/library/service/LoanServiceTest.java
+++ library/src/test/java/com/github/ki3lmigu3l/library/service/LoanServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +18,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -82,5 +84,35 @@ public class LoanServiceTest {
                 .hasMessage("Book already loaned");
 
         Mockito.verify(repository, Mockito.never()).save(savingLoan);
+    }
+
+    @Test
+    @DisplayName("Deve obter as informações de um emprestimo pelo ID")
+    public void getLoanDetailsTest () {
+        Long id = 1l;
+        Loan loan = createLoan();
+        loan.setId(id);
+
+        Mockito.when(repository.findById(id)).thenReturn(Optional.of(loan));
+        Optional<Loan> result = service.getById(id);
+
+        assertThat(result.isPresent()).isTrue();
+        assertThat(result.get().getId()).isEqualTo(id);
+        assertThat(result.get().getCustomer()).isEqualTo(loan.getCustomer());
+        assertThat(result.get().getBook()).isEqualTo(loan.getBook());
+        assertThat(result.get().getLoanDate()).isEqualTo(loan.getLoanDate());
+
+        Mockito.verify(repository).findById(id);
+    }
+
+    public Loan createLoan () {
+        Book book = Book.builder().id(1l).build();
+        String customer = "Ezequiel";
+
+        return Loan.builder()
+                .book(book)
+                .customer(customer)
+                .loanDate(LocalDate.now())
+                .build();
     }
 }
